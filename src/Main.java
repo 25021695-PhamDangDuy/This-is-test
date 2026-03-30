debdd44b3c247925d56450150f561fa39f63ba07
ngodinhdungbn-hue <ngodinhdungbn@gmail.com>
2026-03-30 10:15:35 +0000

    Đây là ChageMain test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,5 @@ void main(){
     System.out.println("Tao là thằng đẹp trai");
     System.out.println("Lần thứ 2");
 
-    System.out.println("Đang ở NewAge");
+    System.out.println("Đang ở ChangeMain");
 }
